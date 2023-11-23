7230956b4a4a4a4f5478fb5ac1d95917a42754b6
Justin Ball <justinball@gmail.com>
2023-11-22 23:50:59 -0700

    Add build process for home page

--- esbuild.mjs
+++ esbuild.mjs
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild';
 import fs from 'node:fs';
 
 const [env] = process.argv.slice(2);
-const entryPoints = ['client/app-init.ts', 'client/app.ts'];
+const entryPoints = ['client/app-init.ts', 'client/app.ts', 'client/home.ts'];
 
 const metafilePlugin = {
   name: 'metafileWriter',
@@ -26,6 +26,33 @@ const metafilePlugin = {
   },
 };
 
+const indexHtmlPath = 'public/index.html';
+const htmlFileWriter = {
+  name: 'htmlFileWriter',
+  setup(build) {
+    build.onEnd((result) => {
+      if (result.errors.length === 0) {
+        Object.keys(result.metafile.outputs).forEach((dest) => {
+          const data = result.metafile.outputs[dest];
+          if (data.entryPoint === 'client/home.ts') {
+            // Read the HTML file
+            const html = fs.readFileSync(indexHtmlPath, 'utf-8');
+
+            // Replace the script src with the new transpiled JavaScript file name
+            const newHtml = html.replace(
+              /<script id="main-script" src=".+.js"><\/script>/g, 
+              `<script id="main-script" src="${dest.replace('public/', '')}"></script>`
+            );
+
+            // Write the new HTML back to the file
+            fs.writeFileSync(indexHtmlPath, newHtml);            
+          }
+        });
+      }
+    });
+  }
+};
+
 const baseConfig = {
   entryPoints,
   bundle: true,
@@ -38,7 +65,7 @@ const baseConfig = {
   logLevel: 'info',
   //splitting: true,
   //format: 'esm',
-  plugins: [metafilePlugin],
+  plugins: [metafilePlugin, htmlFileWriter],
   loader: {
     '.js': 'jsx',
     '.json': 'json',
