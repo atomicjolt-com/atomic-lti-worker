52153421793abfa147a8c7e9bc57eb7ab9cd6e3a
Justin Ball <justinball@gmail.com>
2025-03-25 09:01:37 -0600

    Updated packages

--- src/register.ts
+++ src/register.ts
@@ -1,13 +1,13 @@
 import { HandlePlatformResponse } from "@atomicjolt/lti-endpoints/dist/handlers/dynamic_registration_finish";
-import { ToolConfiguration } from "@atomicjolt/lti-types";
+import { RegistrationConfiguration } from "@atomicjolt/lti-types";
 
 
-export const handlePlatformResponse: HandlePlatformResponse = (platformResponse: ToolConfiguration) => {
+export const handlePlatformResponse: HandlePlatformResponse = (registrationConfiguration: RegistrationConfiguration) => {
   // TODO store client id or deployment id here as needed
   // platformResponse.client_id
   // platformResponse[LTI_TOOL_CONFIGURATION].deployment_id
   console.log('--------------------------------------------------------');
-  console.log('platformResponse', platformResponse);
+  console.log('registrationConfiguration', registrationConfiguration);
 
   return null;
 };
